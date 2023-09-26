913517fae8f2c1b90ee4cb17814a7ee7e5e5d52a
ShivamP2E <144093375+ShivamP2E@users.noreply.github.com>
2023-09-26 18:45:38 +0530

    Add files via upload

---
+++ components/Body/BodySection1.js
@@ -0,0 +1,13 @@
+import './BodySection1.css';
+
+const BodySection1 = () => {
+    return (
+        <>
+            <div className='text-container'>
+                <h1>Welcome To Canvas</h1>
+                <p>Create just what you need for your Perfect Website. Choose from a<br></br> wide range of Elements & simply put them on your own Canvas.</p>
+            </div>
+        </>
+    );
+}
+export default BodySection1;
