a64da9eba0573c4a987e1051b5b7bca4a8d0ea54
shivamP2E <shivam.gairola@mai.io>
2023-09-27 18:21:24 +0530

    Third commit: 5 component added

--- src/components/Feature/Feature3.js
+++ src/components/Feature/Feature3.js
@@ -7,22 +7,100 @@ const Feature3 = () => {
             <div className='container'>
                 <div className='row'>
                     <div className='col-4'>
-                        <div className='Resp-wrap-'>
-                            <div className='resp-wrap-1'>
+                        {/* first Column */}
+                        <div className='Resp-wrap-01'>
+                            <div className='resp-wrap'>
                                 <div className='resp-image'>
+                                    <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1fxRzJkbrzqc_7100-uHUB-0YO396q6bpG-EhV8lM6YQk1pn8' alt='screen'></img>
                                 </div>
                                 <div className='resp-content'>
                                     <h3>Responsive Layout</h3>
                                     <p>Powerful Layout with Responsive functionality<br></br> that can be adapted to any screen size.</p>
                                 </div>
                             </div>
+                            <div className='resp-wrap'>
+                                <div className='resp-image'>
+                                    <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1fxRzJkbrzqc_7100-uHUB-0YO396q6bpG-EhV8lM6YQk1pn8' alt='screen'></img>
+                                </div>
+                                <div className='resp-content'>
+                                    <h3>HTML5 Video</h3>
+                                    <p>Canvas provides support for Native HTML5 Videos that can be added to a Full Width Background.</p>
+                                </div>
+                            </div>
+                            <div className='resp-wrap'>
+                                <div className='resp-image'>
+                                    <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1fxRzJkbrzqc_7100-uHUB-0YO396q6bpG-EhV8lM6YQk1pn8' alt='screen'></img>
+                                </div>
+                                <div className='resp-content'>
+                                    <h3>Light & Dark Color Schemes</h3>
+                                    <p>Change your Website's Primary Scheme instantly by simply adding the dark class to the body.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    {/* Seccond column */}
                     <div className='col-4'>
-
+                    <div className='Resp-wrap-01'>
+                            <div className='resp-wrap'>
+                                <div className='resp-image'>
+                                    <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1fxRzJkbrzqc_7100-uHUB-0YO396q6bpG-EhV8lM6YQk1pn8' alt='screen'></img>
+                                </div>
+                                <div className='resp-content'>
+                                    <h3>Responsive Layout</h3>
+                                    <p>Powerful Layout with Responsive functionality<br></br> that can be adapted to any screen size.</p>
+                                </div>
+                            </div>
+                            <div className='resp-wrap'>
+                                <div className='resp-image'>
+                                    <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1fxRzJkbrzqc_7100-uHUB-0YO396q6bpG-EhV8lM6YQk1pn8' alt='screen'></img>
+                                </div>
+                                <div className='resp-content'>
+                                    <h3>HTML5 Video</h3>
+                                    <p>Canvas provides support for Native HTML5 Videos that can be added to a Full Width Background.</p>
+                                </div>
+                            </div>
+                            <div className='resp-wrap'>
+                                <div className='resp-image'>
+                                    <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1fxRzJkbrzqc_7100-uHUB-0YO396q6bpG-EhV8lM6YQk1pn8' alt='screen'></img>
+                                </div>
+                                <div className='resp-content'>
+                                    <h3>Light & Dark Color Schemes</h3>
+                                    <p>Change your Website's Primary Scheme instantly by simply adding the dark class to the body.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    {/* Thrid Column */}
                     <div className='col-4'>
-
+                    <div className='Resp-wrap-01'>
+                            <div className='resp-wrap'>
+                                <div className='resp-image'>
+                                    <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1fxRzJkbrzqc_7100-uHUB-0YO396q6bpG-EhV8lM6YQk1pn8' alt='screen'></img>
+                                </div>
+                                <div className='resp-content'>
+                                    <h3>Responsive Layout</h3>
+                                    <p>Powerful Layout with Responsive functionality<br></br> that can be adapted to any screen size.</p>
+                                </div>
+                            </div>
+                            <div className='resp-wrap'>
+                                <div className='resp-image'>
+                                    <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1fxRzJkbrzqc_7100-uHUB-0YO396q6bpG-EhV8lM6YQk1pn8' alt='screen'></img>
+                                </div>
+                                <div className='resp-content'>
+                                    <h3>HTML5 Video</h3>
+                                    <p>Canvas provides support for Native HTML5 Videos that can be added to a Full Width Background.</p>
+                                </div>
+                            </div>
+                            <div className='resp-wrap'>
+                                <div className='resp-image'>
+                                    <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR1fxRzJkbrzqc_7100-uHUB-0YO396q6bpG-EhV8lM6YQk1pn8' alt='screen'></img>
+                                </div>
+                                <div className='resp-content'>
+                                    <h3>Light & Dark Color Schemes</h3>
+                                    <p>Change your Website's Primary Scheme instantly by simply adding the dark class to the body.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
