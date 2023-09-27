b9d215d7b89882e40ec851be53c7be8598a3678f
shivamP2E <shivam.gairola@mai.io>
2023-09-27 18:04:28 +0530

    Second commit

---
+++ src/components/Feature/Feature3.js
@@ -0,0 +1,32 @@
+import './Feature3.css';
+// This is third section we have muultiple tiles in 3 coloumn.
+
+const Feature3 = () => {
+    return(
+        <div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-4'>
+                        <div className='Resp-wrap-'>
+                            <div className='resp-wrap-1'>
+                                <div className='resp-image'>
+                                </div>
+                                <div className='resp-content'>
+                                    <h3>Responsive Layout</h3>
+                                    <p>Powerful Layout with Responsive functionality<br></br> that can be adapted to any screen size.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-4'>
+
+                    </div>
+                    <div className='col-4'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Feature3;
